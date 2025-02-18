3f47e2efac85445378c42da45098a7c8ca2aa9b2
DearBird <nanocopito@hotmail.com>
2025-02-18 12:03:48 +0100

    Second commit, some fix

--- L2Ejercicio3.java
+++ L2Ejercicio3.java
@@ -1,18 +1,18 @@
 /* Pide al usuario un número N y muestra solo los números pares hasta N. */
 
-import java.util.Scanner;
+import java.util.Scanner; // Importamos el scanner
 
-public class L2Ejercicio3 {
+public class L2Ejercicio3 { //Nombre del projecto
     public static void main (String [] args) {
-        Scanner leerDatos = new Scanner(System.in);
-        System.out.println("Dame el número ");
+        Scanner leerDatos = new Scanner(System.in); // creamos el scanner y le asignamos un nombre
+        System.out.println("Dame el número "); 
         int n1 = leerDatos.nextInt();
 
-        for (int i = 1; i <= n1; i++) {
-            if (i % 2 == 0) {
+        for (int i = 1; i <= n1; i++) { //Creamos un bucle en el que cremos una nueva variable, si esa variable es menor o igual a 1 le sumamos 1
+            if (i % 2 == 0) { //Si el número asignado es dividido entre 2 y el resto da 0 pasa algo, si no no
                 System.out.println(i);
             } 
         }
-        leerDatos.close();
+        leerDatos.close(); //Cerramos el Scanner
     }
 }
--- L2Ejercicio4.java
+++ L2Ejercicio4.java
@@ -1,7 +1,16 @@
 /* Pide al usuario 5 números y guárdalos en un array. Luego, muéstralos. */
 
+import java.util.Scanner;
+
 public class L2Ejercicio4 {
     public static void main (String [] args) {
+        Scanner leerDatos = new Scanner(System.in);
+        int[] n1 = new int[5];
 
+        for (int i = 1; i < n1.length; i++)
+        {
+            System.out.println("Dame un número ");
+            n1[i] = leerDatos.nextInt();
+        }
     }
 }
