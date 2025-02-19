f3945e5652c71f59adc2a538d3938f7068d8d527
DearBird <nanocopito@hotmail.com>
2025-02-19 12:59:28 +0100

    19-02-2025

--- Ej4Lista.java
+++ Ej4Lista.java
@@ -1,17 +1,21 @@
 /* Pide al usuario 5 números y guárdalos en un array. Luego, muéstralos. */
 
-import java.util.Scanner; // SIN TERMINAR
+import java.util.Scanner; 
 
 public class Ej4Lista {
     public static void main (String [] args) {
         Scanner leerDatos = new Scanner(System.in);
         int[] n1 = new int[5];
 
-        for (int i = 1; i < n1.length; i++)
+        for (int i = 0; i < n1.length; i++)
         {
             System.out.println("Dame un número ");
             n1[i] = leerDatos.nextInt();
         }
+        for (int i = 0; i < n1.length; i++) {
+            System.out.println("Número " + (i + 1) + ": " + n1[i]);
+        }
+        //Necesito hacer muchos ejercicios de Arrays...
         leerDatos.close();
     }
 }
